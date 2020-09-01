f1f31b057694df6c05b20299073888a2f0cbf25e
sheetalrprasad <sheetalraniprasad@gmail.com>
2020-09-01 09:49:22 +0530

    'Special'

--- learning_users/basic_app/forms.py
+++ learning_users/basic_app/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import UserProfileInfo
+from basic_app.models import UserProfileInfo
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget = forms.PasswordInput())
 
     class Meta():
         model = User
         fields = ('username','email','password')
-
 
 class UserProfileInfoForm(forms.ModelForm):
     class Meta():
--- learning_users/basic_app/urls.py
+++ learning_users/basic_app/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from . import views
+from basic_app import views
 
-# SET THE NAMESPACE!
 app_name = 'basic_app'
 
-# Be careful setting the name to just /login use userlogin instead!
-urlpatterns=[
-    path('register/',views.register,name='register'),
+urlpatterns = [
+    path('register',views.register, name= 'register'),
     path('user_login/',views.user_login,name='user_login'),
 ]
